54270eed5c0394225aabcda673e859095ff12286
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2021-07-13 18:56:17 -0700

    Pass state as props, determine season

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // const App = () => {
 //   window.navigator.geolocation.getCurrentPosition(
@@ -25,7 +26,8 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if(!this.state.errorMessage && this.state.lat){
-      return <div>Latitude: {this.state.lat}</div>
+      // return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
     return <div>Loading...</div>
   }  
