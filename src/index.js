3c831e30629c7453fcaf8756a0352dc317abec49
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2021-07-16 22:38:53 -0700

    Update Spinner component

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ class App extends React.Component {
       // return <div>Latitude: {this.state.lat}</div>
       return <SeasonDisplay lat={this.state.lat} />
     }
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }  
 }
 
